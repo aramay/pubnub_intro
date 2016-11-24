6d3e42d19f92215bdebc6c5b509bd5c51744b76f
“kasper341” <kasper_341@hotmail.com>
2016-11-24 10:11:58 -0800

    add code to publish many messages

--- publish.js
+++ publish.js
@@ -3,3 +3,31 @@ PUBNUB_demo.publish({
     channel: 'demo_tutorial',
     message: {"color":"blue"}
  });
+
+
+function pub() {
+
+    for (var i = 0; i < 10; i++) {
+        //publish message i times
+        PUBNUB_demo.publish({
+            channel: 'demo_tutorial',
+            message: "message " + i
+        });
+    }
+}
+
+
+// simple history call with only the two required parameters: the channel (“up103_demo”) and a callback function.
+PUBNUB_demo.history({
+    channel: 'demo_tutorial',
+    callback: function(m){console.log(JSON.stringify(m))}
+});
+
+// add some parameters to narrow down the number of messages we receive. Let’s use the count parameter to return 5 messages as opposed to the default 100.
+
+// count param
+PUBNUB_demo.history({
+    channel: 'up103_demo',
+    callback: function(m){console.log(JSON.stringify(m))},
+    count: 5
+});
